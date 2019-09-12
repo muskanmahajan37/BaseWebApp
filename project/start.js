65cfcddbd5e8f4a6d555c116607b779d9395040f
sunn-e <sunnydhoke22@gmail.com>
2019-09-12 23:17:17 +0530

    Added a bug that is a nice feature called infinite puinches

--- project/start.js
+++ project/start.js
@@ -1,19 +1,25 @@
 var express = require('express');
-var app = express();
+var app = express(); //vreate express app
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000)); //tells port and dir
 
 app.use(express.static(__dirname));
 
 // views is directory for all template files
 app.set('views', __dirname + '/html');
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //tells it uses ejs as templating language
 
-app.get('/', function(request, response) {
+app.get('/', function(request, response) { //index page rendering
   response.render('pages/index');
 });
+app.get('/about', function(request, response) { //index page rendering
+  response.render('pages/about');
+});
+app.get('/projects', function(request, response) { //index page rendering
+  response.render('pages/projects');
+});
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function() { //
   console.log('Node app is running on port', app.get('port'));
 });
 
