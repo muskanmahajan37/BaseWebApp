7b8773ab337b431de6213ea9b47969999a29c663
sunn-e <sunnydhoke22@gmail.com>
2019-09-13 16:22:30 +0530

    Added humidity

--- project/js/main.js
+++ project/js/main.js
@@ -52,7 +52,7 @@ function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
   $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! This was my first web app!");
+  $("p").html("High five! This was my first web app!Click click click for infinite puppies");
 
   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
   
