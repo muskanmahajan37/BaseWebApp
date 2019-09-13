5087a6e30f6a97052bef5e42c4695f20a551641a
sunn-e <sunnydhoke22@gmail.com>
2019-09-13 15:01:01 +0530

    Added custom search bar

--- project/js/main.js
+++ project/js/main.js
@@ -1,32 +1,50 @@
 $(document).ready(function(){
   getWeather();
 })
-
+/* for constant display inititilly*/
 function getWeather(){
   var url = "https://api.openweathermap.org/data/2.5/weather?q=Nagpur&units=metric&APPID="+apiKey;
 
+  $(".city").text(""); //to clean the output on page
+  $(".temp").text("");
+
   $.ajax(url,{success: function(data){
     console.log(data);//to show response on console
     
     $(".city").text(data.name);
-    $(".temp").text(data.main.temp);
+    $(".temp").text(data.main.temp+" Celcius");
   }})
 }
+
+
+
 function getWeatherbySearch(searchQuery){
   var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=metric&APPID="+apiKey;
+  
+  $(".city").text(""); //to clean the output on page
+  $(".temp").text("");
+  $(".error-message").text("")
 
   $.ajax(url,{success: function(data){
     console.log(data);//to show response on console
     
     $(".city").text(data.name);
-    $(".temp").text(data.main.temp);
+    $(".temp").text(data.main.temp+" Celcius");
+  }, error: function(error){
+
+    $(".error-message").text("oh oh,City not found..!")
   }})
-}
+  }
+
 
 function searchWeather(){
+
   var searchQuery = $(".search").val();
   getWeatherbySearch(searchQuery);
 }
+
+
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
